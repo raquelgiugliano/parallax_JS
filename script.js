b5a397bd5f9b2793a19e7016b32ccb323a7b47a6
raquelgiugliano <raquel.giugliano@gmail.com>
2023-06-05 14:41:44 +0200

    Parallax

---
+++ script.js
@@ -0,0 +1,5 @@
+var scene = document.getElementById("scene");
+var text = document.getElementById("text");
+
+var parallaxInstanceScene = new Parallax(scene);
+var parallaxInstanceText = new Parallax(text);
